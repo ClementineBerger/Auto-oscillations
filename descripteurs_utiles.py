d5b5fdad3cc31216103594c7f7a31027e4b0653f
malenafouillou <114601777+malenafouillou@users.noreply.github.com>
2023-02-02 23:01:59 +0100

    Mapping_modele_clarinette

---
+++ descripteurs_utiles.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 12:32:49 2023
+
+@author: fouilloumalena
+"""
+
+"""
+Ce script contient les fonctions calculant les descripteurs utiles au mapping
+
+"""
+
+import os
+import scipy.io as io
+from scipy.io import savemat
+import librosa
+import librosa.display
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Son ou pas son ?
+def are_there_oscillations(waveform, epsilon):
+    """
+    Parameters
+    ----------
+    waveform = p(t) calculé à partir d'un modèle
+    arg_d = critère de décision
+
+    Returns
+    -------
+    True or False selon la condition : y-a-t-il oscillation ou non ?
+    """
+    
+    N2_3 = int(len(waveform) / 3)
+    criterion = np.mean(np.abs(waveform[N2_3:]))
+    return criterion > epsilon
+
+
+def get_f0(waveform, sr, fmin=librosa.note_to_hz("C2"), fmax=librosa.note_to_hz("C7")):
+    """ Estimate F0 using Yin's algorithm. The algorithm is applied frame by frame
+    then the final F0 is obtained using the mean of the last 2/3 frames."""
+    n_1_3 = int(np.rint(len(waveform) / 3))
+    f0 = librosa.yin(waveform[n_1_3:], fmin=fmin, fmax=fmax, sr=sr)
+    return np.mean(f0[1:])
+
+
+def f0_to_categorical(f0, ref_frequencies, epsilon):
+    """ This function checks whether f0 is close to a frequency in the ref_frequencies
+    array, close meaning less than epsilon cents away.
+
+    Args:
+        f0 (float): f0 to test
+        ref_frequencies (array): Array of reference frequencies to be checked
+        epsilon (int): Theshold in cents
+    """
+    n_cents = np.abs(1200 * np.log(f0 / ref_frequencies))
+    idx = np.argmin(n_cents)
+    if n_cents[idx] > epsilon:
+        return False, None
+    else:
+        return True, idx
+
+ 
---
+++ run_simulation.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 12:09:24 2023
+
+@author: fouilloumalena
+"""
+"""
+
+This code takes as arguments a mapping of points (X) and the observed descriptor
+('oscillations', 'pitch', 'attack' etc...)
+                                                                                                   
+It returns an array of labels y of X.
+
+WE NEED TO BE ABLE TO MOVE ON FROM THE ARGUMENTS OF MAT
+
+"""
+import descripteurs_utiles as dp
+import os
+import pandas as pd
+from tqdm.auto import tqdm
+import numpy as np
+import librosa
+
+def labels(X, descriptor, abscisse, ordonnee, modele, arg_modele, arg_descriptor, note_frequencies):
+    
+    """
+    Parameters
+    
+    X = points du mapping
+    
+    descriptor : descritpeur que l'on choisit d'observer
+    
+    abscisse, ordonnée : paramètre du mappping ("gamma/zeta", ou "gamma/longueur" etc...)
+    
+    modele= fonction de simulation du modele choisit pour le mapping : méthode guide d'onde ou modale
+    
+    arg_descriptors = arguments des fonctions descripteurs
+    
+    arg_model = arguments des méthodes
+    
+    Returns
+    -------
+    y = tableau contenant la classe de chaque point
+    
+    """
+    
+    #Conversion mat to Python
+    arg_modele= np.array(arg_modele).tolist()
+    note_frequencies = np.array(note_frequencies).tolist()
+    arg_descriptor= np.array(arg_descriptor).tolist()
+    X= pd.DataFrame(np.array(X), columns = [abscisse, ordonnee])   
+   
+    
+    # Create labels
+    y = np.zeros(len(X))
+    
+    #Méthode utilisée
+    if modele == 'guide_onde': #Paramètres des méthode
+        from modelisation_physique.guide_onde import simulation
+    else :
+        from modelisation_physique.Modele_modal_fct import simulation
+        
+    t_max, fe, L, c = arg_modele
+
+    # Descripteur choisit
+    if descriptor == "are_there_oscillations": #Paramètres des descripteurs
+        epsilon = arg_descriptor
+        type_reflection='dirac'
+        for i, x in tqdm(X.iterrows()):
+            waveform, _ = simulation(x[abscisse], x[ordonnee], t_max, fe, L, c)
+            y[i] = 1 if dp.are_there_oscillations(waveform, epsilon) else 0
+        return X, y
+
+    
+    elif descriptor == "pitch" :
+        n_classes = len(note_frequencies)
+        fe ,osc_threshold ,cents_threshold, zeta, freq = arg_descriptor
+        for i, x in tqdm(X.iterrows()):
+            waveform, _ = simulation(x[abscisse], zeta, t_max, fe, x[ordonnee], c)  
+            f0 = dp.get_f0(waveform, fe) * dp.are_there_oscillations(waveform, osc_threshold)
+            is_close, idx = dp.f0_to_categorical(f0, note_frequencies, epsilon=cents_threshold)
+            if is_close:
+                y[i] = idx
+            else:
+                y[i] = n_classes
+
+        for i in range(len(y)):
+            if y[i] !=freq : 
+                y[i] = 0
+        return X, y
+
+X, y =  labels(X, descriptor, abscisse, ordonnee, modele, arg_modele, arg_descriptor, note_frequencies)
+
