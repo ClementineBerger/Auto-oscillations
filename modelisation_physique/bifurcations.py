9d9659f8316b2400afbbb8a4009460d0cdaea47f
caronromain <114607985+caronromain@users.noreply.github.com>
2023-02-03 14:15:22 +0100

    Plus de clarté

--- modelisation_physique/bifurcations.py
+++ modelisation_physique/bifurcations.py
@@ -14,21 +14,27 @@ import Modele_modal_fct_rampe #Importer le modèle à analyser
 
 plt.close(100)
 
-fs=44100
-tmax=1
-zeta=0.8
+#------------------------------------------------------Paramètres
+sample_rate=44100           #Fréquence d'echt
+tmax=0.5                     #Durée de chaque simulation
+zeta_force=0.8              #Valeur de zeta ou de la force d'excitation
+gamma_min=0;                #Première valeur de gamma / vitesse
+gamma_max=2;                #Dernière valeur de gamma / vitesse
+gamma_step=0.03;            #Pas de gamma / vitesse
 
-gammas_bif=np.arange(8,20,1)
+
+#------------------------------------------------------Initialisation
+gammas_bif=np.arange(gamma_min,gamma_max,gamma_step) 
 f_sortie=np.zeros(len(gammas_bif))
 i=0
+#------------------------------------------------------Boucle sur gamma/vitesse
 for g in gammas_bif:                                
-    p,time = Modele_modal_fct_rampe.simulation(dur=tmax,nb_mode=2, fs=fs, gamma=g, zeta=zeta, L=60e-2,c=340,rc=2e-2,fig=False, sound=False) #Simulation
-    p[abs(p) < 1e-5] = 1e-5;
-    result = argrelextrema(p[int(len(p)-fs*0.1*tmax):], np.greater, order =15) #Obtention des maxima
+    p,time = Modele_modal_fct_rampe.simulation(tmax=tmax,nb_mode=5,instrument='clarinette', sample_rate=sample_rate, gamma_velo=g, zeta_force=zeta_force,durete_rampe=2000, l_resonateur=60e-2,fig=False, sound=False) #Lancement du modèle désiré
+    p[abs(p) < 1e-5] = 1e-5; #Seuillage pour ne pas prendre en compte les oscillations à très faible amplitude
+    maxima = argrelextrema(p[int(len(p)-sample_rate*0.1*tmax):], np.greater, order =15) #Obtention des maxima
     
-    if result[0].size>0:
-        #f_sortie[i]=1/((result[0][-1]-result[0][0])/((len(result[0])-1)*fs)) #Calcul de la fréquence et stockage
-        f_sortie[i]=librosa.yin(p,fmin=0.5,fmax=20000,sr=44100)[-1]
+    if maxima[0].size>0: #Si il existe au moins un maximum
+        f_sortie[i]=librosa.yin(p,fmin=0.5,fmax=20000,sr=44100)[-1] #Algorithme de Yin pour obtenir la fréquence fondamentale
     i=i+1
     
 #-------------------------------------------------------Plots
